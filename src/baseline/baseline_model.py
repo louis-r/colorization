first attempt to make the model work: incorrect dimensions

src/baseline/baseline_model.py
@@ -131,14 +131,25 @@ def build_baseline_model_v2(input_tf, pts_in_hull_tf, batch_size):
         #                                strides=2,
         #                                activation=tf.nn.relu,
         #                                name='conv_{}_1_deconvolution'.format(8))
+        x = tf.layers.conv2d_transpose(inputs=x,
+                                       filters=256,
+                                       kernel_size=25,
+                                       strides=7,
+                                       activation=tf.nn.relu)
 
+        # 34, 34, 512 -> 56, 56, 256
+        # filter_deconv = tf.Variable(tf.random_normal([34, 34, 256, 512]))
+        # x = tf.nn.conv2d_transpose(value=x,
+        #                            filter=filter_deconv,
+        #                            output_shape=tf.constant([batch_size, 56, 56, 256]),
+        #                            strides=[1, 2, 2, 1],
+        #                            padding='VALID')
         # 34, 34, 512 -> 64, 64, 256
-        filter_deconv = tf.Variable(tf.random_normal([34, 34, 256, 512]))
-        x = tf.nn.conv2d_transpose(value=x,
-                                   filter=filter_deconv,
-                                   output_shape=tf.constant([batch_size, 64, 64, 256]),
-                                   strides=[1, 2, 2, 1],
-                                   padding='VALID')
+        # x = tf.nn.conv2d_transpose(value=x,
+        #                            filter=filter_deconv,
+        #                            output_shape=tf.constant([batch_size, 64, 64, 256]),
+        #                            strides=[1, 2, 2, 1],
+        #                            padding='VALID')
         for j in range(2, 4):
             # Padding of 1
             x = return_padded(x=x)
@@ -178,7 +189,7 @@ def build_baseline_model_v2(input_tf, pts_in_hull_tf, batch_size):
         # Tmp
         # x = tf.reshape(x, [-1, 28 * 28 * 2])
         # Softmax layer
-        # softmax = tf.nn.softmax(logits=x,
+        # softmax = tf.nn.softmax(logits_tf=x,
         #                         name='softmax_{}'.format(9))
 
     return logits, z, class8_ab

src/baseline/load_data.py
@@ -3,13 +3,14 @@
 Load data helpers
 """
 # pylint: disable=invalid-name, redefined-outer-name
+import os
+import sys
 import numpy as np
+# noinspection PyUnresolvedReferences
+from image_utils import convert_image_Qspace
 
 
-# input: path to the different files
-# output: X_train, y_train, X_l
-
-def load_data(a_file, b_file, L_file, gray_file):
+def load_data(a_file, b_file, L_file, **kwargs):
     """
     Docstring @PH
     Args:
@@ -21,19 +22,36 @@ def load_data(a_file, b_file, L_file, gray_file):
     Returns:
 
     """
+
+    NN = kwargs.pop('NN_ ', 10.)
+    sigma = kwargs.pop('sigma_ ', 5.)
+    gamma = kwargs.pop('gamma_ ', .5)
+    alpha = kwargs.pop('alpha_ ', 1.)
+
     X_a = np.load(a_file)
     X_b = np.load(b_file)
     X_l = np.load(L_file)
-    X_gray = np.load(gray_file)
+    # X_gray = np.load(gray_file)
 
-    X_train = np.zeros((X_gray.shape[0], X_gray.shape[1], 1))
-    X_train[:, :, 0] = X_gray
+    n_images = 1
+    print('Subsetting to the first {} images'.format(n_images))
+    X_a = X_a[:n_images, :]
+    X_b = X_b[:n_images, :]
+    X_l = X_l[:n_images, :]
 
-    y_train = np.zeros((X_gray.shape[0], X_gray.shape[1], 2))
-    y_train[:, :, 0] = X_a
-    y_train[:, :, 1] = X_b
+    lab_ab = np.zeros((X_l.shape[0], X_l.shape[1], 2))
+    lab_ab[:, :, 0] = X_a
+    lab_ab[:, :, 1] = X_b
 
-    return X_train, y_train, X_l
+    # Reshape
+    lab_ab = lab_ab.reshape(-1, 256, 256, 2)
+    lab_ab = lab_ab.transpose((0, 3, 1, 2))  # N, 3, H, W
+    X_l = X_l.reshape(-1, 256, 256, 1)
+
+    prior_Qimage, Q_image = convert_image_Qspace(lab_ab=lab_ab, NN=NN, sigma=sigma, gamma=gamma, alpha=alpha,
+                                                 ENC_DIR='')
+
+    return X_l, Q_image
 
 
 # creates a dictionary of batches of data

src/baseline/train.py
@@ -9,16 +9,19 @@ import numpy as np
 import tensorflow as tf
 # noinspection PyUnresolvedReferences
 from baseline_model import build_baseline_model_v2
+# noinspection PyUnresolvedReferences
+from load_data import load_data, dict_batch
+import scipy.ndimage.interpolation as sni
 
 tf.app.flags.DEFINE_string('train_dir', 'runs/',
                            """Output dir for tensorflow summaries.""")
-tf.app.flags.DEFINE_string('prefix', 'prefix',
+tf.app.flags.DEFINE_string('prefix', None,
                            """Model name prefix.""")
 tf.app.flags.DEFINE_float('learning_rate', 1e-2,
                           """Learning rate.""")
 tf.app.flags.DEFINE_integer('n_steps', 10,
                             """Number of training epochs.""")
-tf.app.flags.DEFINE_integer('batch_size', 20,
+tf.app.flags.DEFINE_integer('batch_size', 1,
                             """Batch size.""")
 
 FLAGS = tf.app.flags.FLAGS
@@ -29,54 +32,68 @@ learning_rate = FLAGS.learning_rate
 n_steps = FLAGS.n_steps
 batch_size = FLAGS.batch_size
 
-model_name = '{}_{}_{}_{}'.format(prefix,
-                                  learning_rate,
-                                  n_steps,
-                                  batch_size)
-
 H_in, W_in = 256, 256
+H_out, W_out = 256, 256
 Q = 313
+
+# Load the data
+X_l, Qimage = load_data(a_file='../../data/X_lab_a0.npy',
+                        b_file='../../data/X_lab_b0.npy',
+                        L_file='../../data/X_lab_L0.npy')
+L = X_l.reshape(-1, H_in, W_in, 1)
+z_true = Qimage.reshape(-1, H_in, W_in, Q)
+
+model_name = 'lr={}_n_steps={}_batch_size={}'.format(learning_rate,
+                                                     n_steps,
+                                                     batch_size)
+if prefix is not None:
+    model_name = '{}_{}'.format(prefix, model_name)
+
 # Do not specify the size of the training batch
 L_tf = tf.placeholder(tf.float32, [None, H_in, W_in, 1], name='L_tf')
+tf.summary.image('L_tf', L_tf, max_outputs=3)
 # 56 shape from caffe_v1.txt
 T_recip_tf = tf.placeholder(tf.float32, [None, 56, 56, Q], name='T_recip_tf')
 # Cluster centers
 pts_in_hull_tf = tf.placeholder(tf.float32, [Q, 2], name='pts_in_hull_tf')
 
-tf.summary.image('L_tf', L_tf, max_outputs=3)
 
 # TODO incorrect output shape for now
+# The output shape of y_pred_tf should be (56, 56)
+# cf: https://github.com/richzhang/colorization/blob/master/colorization/demo/colorization_demo_v2.ipynb
 # Predicted values
-logits, Z_pred, y_pred = build_baseline_model_v2(input_tf=L_tf,
-                                                 pts_in_hull_tf=pts_in_hull_tf,
-                                                 batch_size=batch_size)
+logits_tf, Z_pred_tf, y_pred_tf = build_baseline_model_v2(input_tf=L_tf,
+                                                          pts_in_hull_tf=pts_in_hull_tf,
+                                                          batch_size=batch_size)
 
 # Display image
-# tf.summary.image('y_pred_a', tf.expand_dims(input=Z_pred[:, :, :, 0], axis=3), max_outputs=3)
-# tf.summary.image('y_pred_b', tf.expand_dims(input=Z_pred[:, :, :, 1], axis=3), max_outputs=3)
+# tf.summary.image('y_pred_a', tf.expand_dims(input=Z_pred_tf[:, :, :, 0], axis=3), max_outputs=3)
+# tf.summary.image('y_pred_b', tf.expand_dims(input=Z_pred_tf[:, :, :, 1], axis=3), max_outputs=3)
+complete_image_tf = tf.concat([y_pred_tf, L_tf], axis=3)
+tf.summary.image('complete_image_tf', complete_image_tf[:, :, :, :], max_outputs=1)
 
-H_out, W_out = y_pred.get_shape().as_list()[1:3]
-assert H_out == W_out == 64, 'Incorrect output shapes'
+# H_out, W_out = y_pred_tf.get_shape().as_list()[1:3]
+# assert H_out == W_out == 64, 'Incorrect output shapes'
 
 # Ground truth
-y_true = tf.placeholder(tf.float32, [None, H_out, W_out, 2], name='y_true')
+y_true_tf = tf.placeholder(tf.float32, [None, H_out, W_out, 2], name='y_true_tf')
 # Z_true hard-encoding of the ground_truth color
-z_true = tf.placeholder(tf.float32, [None, H_out, W_out, Q], name='z_true')
+z_true_tf = tf.placeholder(tf.float32, [None, H_out, W_out, Q], name='z_true_tf')
 
 print('L_tf shape = {}'.format(L_tf.get_shape().as_list()))
 print('T_recip_tf shape = {}'.format(T_recip_tf.get_shape().as_list()))
-print('logits shape = {}'.format(logits.get_shape().as_list()))
-print('Z_pred shape = {}'.format(Z_pred.get_shape().as_list()))
-print('y_pred shape = {}'.format(y_pred.get_shape().as_list()))
-print('y_true shape = {}'.format(y_true.get_shape().as_list()))
-print('z_true shape = {}'.format(z_true.get_shape().as_list()))
+print('logits_tf shape = {}'.format(logits_tf.get_shape().as_list()))
+print('Z_pred_tf shape = {}'.format(Z_pred_tf.get_shape().as_list()))
+print('y_pred_tf shape = {}'.format(y_pred_tf.get_shape().as_list()))
+print('y_true_tf shape = {}'.format(y_true_tf.get_shape().as_list()))
+print('z_true_tf shape = {}'.format(z_true_tf.get_shape().as_list()))
 
 # Metrics
 with tf.name_scope("loss"):
-    # loss = tf.reduce_mean(2 * tf.nn.l2_loss(t=y_true - y_pred),
+    # loss = tf.reduce_mean(2 * tf.nn.l2_loss(t=y_true_tf - y_pred_tf),
     #                       name='reduced_l2_loss')
-    loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=z_true,
-                                           logits=logits)
+    loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=z_true_tf,
+                                           logits=logits_tf)
     loss_summary_tf = tf.summary.scalar("loss", loss)
 
 global_step = tf.Variable(0, trainable=False, name='global_step')
@@ -114,14 +131,14 @@ with tf.Session() as sess:
 
     for step in range(n_steps):
         # Train
-        batch_x = np.random.rand(batch_size, H_in, W_in, 1)
-        # batch_y = np.random.rand(batch_size, H_out, W_out, 2)
-        batch_z = np.random.randint(low=0, high=2, size=(batch_size, H_out, W_out, Q))
-        _, train_loss_val, s = sess.run(
-            [train_step, loss, train_summaries_tf],
+        batch_x = L
+        batch_z = z_true
+
+        _, train_loss_val, s, y_pred_val = sess.run(
+            [train_step, loss, train_summaries_tf, y_pred_tf],
             feed_dict={
                 L_tf: batch_x,
-                z_true: batch_z,
+                z_true_tf: batch_z,
                 pts_in_hull_tf: np.load('pts_in_hull.npy').astype(float)
             })
 
@@ -129,21 +146,21 @@ with tf.Session() as sess:
 
         print('TRAIN Step = %04d\tloss = %.4f' % (step + 1,
                                                   train_loss_val))
-        # Cross validate
-        test_batch_x = np.random.rand(batch_size, H_in, W_in, 1)
-        # test_batch_y = np.random.rand(batch_size, H_out, W_out, 2)
-        test_batch_z = np.random.randint(low=0, high=2, size=(batch_size, H_out, W_out, Q))
-        test_loss_val, s = sess.run(
-            [loss, test_summaries_tf],
-            feed_dict={
-                L_tf: test_batch_x,
-                z_true: test_batch_z,
-                pts_in_hull_tf: np.load('pts_in_hull.npy').astype(float)
-            })
-        print('TEST Step = %04d\tloss = %.4f' % (step + 1,
-                                                 test_loss_val))
+        # # Cross validate
+        # test_batch_x = np.random.rand(batch_size, H_in, W_in, 1)
+        # # test_batch_y = np.random.rand(batch_size, H_out, W_out, 2)
+        # test_batch_z = np.random.randint(low=0, high=2, size=(batch_size, H_out, W_out, Q))
+        # test_loss_val, s = sess.run(
+        #     [loss, test_summaries_tf],
+        #     feed_dict={
+        #         L_tf: test_batch_x,
+        #         z_true_tf: test_batch_z,
+        #         pts_in_hull_tf: np.load('pts_in_hull.npy').astype(float)
+        #     })
+        # print('TEST Step = %04d\tloss = %.4f' % (step + 1,
+        #                                          test_loss_val))
 
-        test_writer.add_summary(s, step)
+        # test_writer.add_summary(s, step)
 
 print("Run the command line:\n"
       "--> tensorboard --logdir={} "