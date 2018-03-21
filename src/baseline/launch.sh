779cd3faf2db7bf95778fcca498325e48bc9575a
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2018-03-20 18:57:44 -0700

    Reorganized

--- src/baseline/launch.sh
+++ src/baseline/launch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 train.py --batch_size=3 --n_steps=10 --prefix=transpose
+python3 train.py --batch_size=3 --n_steps=20 --prefix=reorg
--- src/baseline/train.py
+++ src/baseline/train.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 # noinspection PyUnresolvedReferences
 from baseline_model import build_baseline_model_v2
 # noinspection PyUnresolvedReferences
-from load_data import load_data
+from data_utils import load_data
 from image_utils import lab_to_rgb
 
 tf.app.flags.DEFINE_string('train_dir', 'runs/',
@@ -24,7 +24,13 @@ tf.app.flags.DEFINE_integer('n_steps', 1000,
                             """Number of training epochs.""")
 tf.app.flags.DEFINE_integer('batch_size', 1,
                             """Batch size.""")
+tf.app.flags.DEFINE_integer('image_summ_freq', 10,
+                            """Frequency of image summaries.""")
+tf.app.flags.DEFINE_integer('var_summ_freq', 10,
+                            """Frequency of var summaries.""")
 
+# MODEL SPECIFICATION
+# Parse flags argument
 FLAGS = tf.app.flags.FLAGS
 
 train_dir = FLAGS.train_dir
@@ -32,19 +38,8 @@ prefix = FLAGS.prefix
 learning_rate = FLAGS.learning_rate
 n_steps = FLAGS.n_steps
 batch_size = FLAGS.batch_size
-
-H_in, W_in = 256, 256
-H_out, W_out = 256, 256
-Q = 313
-
-# Load the data
-L, z_true, y_true = load_data(a_file='../../data/X_lab_a0.npy',
-                              b_file='../../data/X_lab_b0.npy',
-                              L_file='../../data/X_lab_L0.npy',
-                              n_images=batch_size)
-assert L.shape == (batch_size, H_in, W_in, 1)
-assert y_true.shape == (batch_size, H_in, W_in, 2)
-assert z_true.shape == (batch_size, H_in, W_in, Q)
+image_summ_freq = FLAGS.image_summ
+var_summ_freq = FLAGS.var_summ
 
 model_name = 'lr={}_n_steps={}_batch_size={}'.format(learning_rate,
                                                      n_steps,
@@ -53,7 +48,23 @@ if prefix is not None:
     model_name = '{}_{}'.format(prefix, model_name)
 
 print('Training {} at {}'.format(model_name, datetime.datetime.now()))
-# Do not specify the size of the training batch
+
+H_in, W_in = 256, 256
+H_out, W_out = 256, 256
+Q = 313
+
+######## DATA ########
+# Load the data
+L, z_true, y_true = load_data(a_file='../../data/X_lab_a0.npy',
+                              b_file='../../data/X_lab_b0.npy',
+                              L_file='../../data/X_lab_L0.npy',
+                              n_images=batch_size)
+# Checks
+assert L.shape == (batch_size, H_in, W_in, 1)
+assert y_true.shape == (batch_size, H_in, W_in, 2)
+assert z_true.shape == (batch_size, H_in, W_in, Q)
+
+######## TENSORFLOW GRAPH ########
 L_tf = tf.placeholder(tf.float32, [None, H_in, W_in, 1], name='L_tf')
 # 56 shape from caffe_v1.txt
 T_recip_tf = tf.placeholder(tf.float32, [None, 56, 56, Q], name='T_recip_tf')
@@ -76,6 +87,9 @@ y_true_tf = tf.placeholder(tf.float32, [None, H_out, W_out, 2], name='y_true_tf'
 # Z_true hard-encoding of the ground_truth color
 z_true_tf = tf.placeholder(tf.float32, [None, H_out, W_out, Q], name='z_true_tf')
 
+recolorized_image_tf = tf.concat([L_tf, y_pred_tf], axis=3, name='recolorized_image_tf')
+original_image_tf = tf.concat([L_tf, y_true_tf], axis=3, name='original_image_tf')
+
 print('L_tf shape = {}'.format(L_tf.get_shape().as_list()))
 print('T_recip_tf shape = {}'.format(T_recip_tf.get_shape().as_list()))
 print('logits_tf shape = {}'.format(logits_tf.get_shape().as_list()))
@@ -83,45 +97,47 @@ print('Z_pred_tf shape = {}'.format(Z_pred_tf.get_shape().as_list()))
 print('y_pred_tf shape = {}'.format(y_pred_tf.get_shape().as_list()))
 print('y_true_tf shape = {}'.format(y_true_tf.get_shape().as_list()))
 print('z_true_tf shape = {}'.format(z_true_tf.get_shape().as_list()))
+print('original_image_tf shape = {}'.format(original_image_tf.get_shape().as_list()))
+print('recolorized_image_tf shape = {}'.format(recolorized_image_tf.get_shape().as_list()))
 
 # Metrics
 with tf.name_scope("loss"):
-    # loss = tf.reduce_mean(2 * tf.nn.l2_loss(t=y_true_tf - y_pred_tf),
-    #                       name='reduced_l2_loss')
     loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=z_true_tf,
                                            logits=logits_tf)
-    loss_summary_tf = tf.summary.scalar("loss", loss)
 
 global_step = tf.Variable(0, trainable=False, name='global_step')
 
 # Optimizer
 with tf.name_scope("optimizer"):
-    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,
-                                                                           global_step=global_step)
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss,
+                                                                global_step=global_step)
 
-# Display images
-max_outputs = min(batch_size, 3)
+######## TENSORFLOW SUMMARIES ########
+# Scalars
+loss_summary_tf = tf.summary.scalar("loss", loss)
+all_scalar_summaries = [loss_summary_tf]
+
+# Images
+max_outputs = min(batch_size, 3)  # Number of images per batch to display
 L_summary = tf.summary.image(L_tf.name,
                              L_tf,
                              max_outputs=max_outputs)
-recolorized_image_tf = tf.concat([L_tf, y_pred_tf], axis=3, name='recolorized_image_tf')
 recolorized_image_summary = tf.summary.image(recolorized_image_tf.name,
                                              lab_to_rgb(recolorized_image_tf),
                                              max_outputs=max_outputs)
-original_image_tf = tf.concat([L_tf, y_true_tf], axis=3, name='original_image_tf')
 original_image_summary = tf.summary.image(original_image_tf.name,
                                           lab_to_rgb(original_image_tf),
                                           max_outputs=max_outputs)
+all_image_summaries = [L_summary, recolorized_image_summary, original_image_summary]
 
-# Create summaries to visualize weights
-var_summaries = []
+# Variables summaries
+all_var_summaries = []
 for var in tf.trainable_variables():
-    var_summaries.append(tf.summary.histogram(var.name, var))
+    all_var_summaries.append(tf.summary.histogram(var.name, var))
 
-# train_summaries_tf = tf.summary.merge_all()
-train_summaries_tf = tf.summary.merge(var_summaries + [loss_summary_tf])
-test_summaries_tf = tf.summary.merge([loss_summary_tf])
-image_summaries_tf = tf.summary.merge([L_summary, recolorized_image_summary, original_image_summary])
+scalar_summaries_op = tf.summary.merge(all_scalar_summaries)
+var_summaries_op = tf.summary.merge(all_var_summaries)
+image_summaries_op = tf.summary.merge(all_image_summaries)
 
 train_write_path = train_dir + model_name + '/train'
 test_write_path = train_dir + model_name + '/test'
@@ -154,15 +170,21 @@ with tf.Session() as sess:
             y_true_tf: batch_y,
             pts_in_hull_tf: np.load('pts_in_hull.npy').astype(float),
         }
-        _, train_loss_val, train_summaries, y_pred_val = sess.run(
-            [train_step, loss, train_summaries_tf, y_pred_tf],
+        _, train_loss_val, scalar_summaries, y_pred_val = sess.run(
+            [train_step, loss, scalar_summaries_op, y_pred_tf],
             feed_dict=train_feed_dict)
 
-        train_writer.add_summary(train_summaries, step)
+        train_writer.add_summary(scalar_summaries, step)
+
+        # Also log var summaries
+        if step % var_summ_freq == 0:
+            var_summaries = sess.run(var_summaries_op,
+                                     feed_dict=train_feed_dict)
+            train_writer.add_summary(var_summaries, step)
 
         # Also log image summaries
-        if step % 10 == 0:
-            image_summary = sess.run(image_summaries_tf,
+        if step % image_summ_freq == 0:
+            image_summary = sess.run(image_summaries_op,
                                      feed_dict=train_feed_dict)
             train_writer.add_summary(image_summary, step)
 
