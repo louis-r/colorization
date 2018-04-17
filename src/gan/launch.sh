a4b0da938b7813db0d3e1133d1a213089209fc14
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2018-04-16 17:39:58 -0700

    reorganized data files

--- src/gan/launch.sh
+++ src/gan/launch.sh
@@ -1,2 +1,2 @@
-python gan_main.py Spongebob/ --dataset bob --batch_size 32 --lr 1e-3 --num_epoch 50 --lamb 100 -s --gpu 0 -p
+python gan_main.py data/Spongebob/ --dataset bob --batch_size 32 --lr 1e-3 --num_epoch 50 --lamb 100 -s --gpu 0 -p
 
